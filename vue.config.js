555c3ceb3e2415b2add27d7b0bbee091732e1061
Andrey Nikonov <a.einomi@gmail.com>
2022-04-28 11:18:28 +0300

    Add a possibility to turn off hot reload

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
+const webpack = require("webpack");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/",
@@ -8,5 +10,22 @@ module.exports = defineConfig({
         additionalData: `@import "~@/sass/styles.scss";`,
       },
     },
+  },
+  configureWebpack: {
+    mode: process.env.NODE_ENV === "production" ? "production" : "development",
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: "process/browser",
+      }),
+    ],
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap((options) => {
+        options.hotReload = !process.env.DISABLE_HOT_RELOAD;
+        return options;
+      });
   },
 });
